98e98b69df07d895174dabbd04ce1687b909b769
Jo996 <julia.ulca@gmail.com>
2022-06-14 18:44:11 +0300

    first practice

---
+++ script.js
@@ -0,0 +1,20 @@
+const numberOfFilms = +prompt('How much films did you watch?', '');
+
+
+const personalMovieDB = {
+    count: personalMovieDB,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('One last watched film', ''),
+    b = prompt('how mush would you rate it?', ''),
+    c = prompt('One last watched film', ''),
+    d = prompt('how mush would you rate it?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
