463a1addd6da0f083aa15cec85d2cd0fbc1243cb
Jo996 <julia.ulca@gmail.com>
2022-06-15 11:57:39 +0300

    conditions and cycles

--- script.js
+++ script.js
@@ -1,20 +1,37 @@
+'use strict';
+
 const numberOfFilms = +prompt('How much films did you watch?', '');
 
 
 const personalMovieDB = {
-    count: personalMovieDB,
+    count: numberOfFilms,
     movies: {},
     actors: {},
     genres: [],
     privat: false
 };
 
-const a = prompt('One last watched film', ''),
-    b = prompt('how mush would you rate it?', ''),
-    c = prompt('One last watched film', ''),
-    d = prompt('how mush would you rate it?', '');
+for (let i = 0; i < 2; i++) {
+    const a = prompt('One last watched film', ''),
+          b = prompt('How much would you rate it?', '');
+    if  (a != null && b != null && a != '' && b!= '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        --i;
+    }
+}
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+if (personalMovieDB.count < 10) {
+    console.log('few movies watched');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('classic viewer');
+} else if (personalMovieDB.count >= 30) {
+    console.log('You are a cinephile');
+} else {
+    console.log('Error');
+}
+
 
 console.log(personalMovieDB);
