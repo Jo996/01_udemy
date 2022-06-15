14013cd0621784c7a4d0e98180c77beffb9de8e3
Jo996 <julia.ulca@gmail.com>
2022-06-15 17:43:36 +0300

    exercise cycles

--- cycles.js
+++ cycles.js
@@ -11,7 +11,8 @@ const personalMovieDB = {
     privat: false
 };
 
-/* for (let i = 0; i < 2; i++) {
+
+for (let i = 0; i < 2; i++) {
     const a = prompt('One last watched film', ''),
           b = prompt('How much would you rate it?', '');
     if  (a != null && b != null && a != '' && b!= '' && a.length < 50) {
@@ -21,23 +22,14 @@ const personalMovieDB = {
         console.log('error');
         --i;
     }
-} */
+}
+
+
 let a = prompt('One last watched film', ''),
     b = prompt('How much would you rate it?', '');
 while (a != null && b != null && a != '' && b != '' && a.length < 50) {
-        personalMovieDB.movies[a] = b;
         console.log('done');
     }
-
-if (personalMovieDB.count < 10) {
-    console.log('few movies watched');
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    console.log('classic viewer');
-} else if (personalMovieDB.count >= 30) {
-    console.log('You are a cinephile');
-} else {
-    console.log('Error');
-}
 
 
 console.log(personalMovieDB);
